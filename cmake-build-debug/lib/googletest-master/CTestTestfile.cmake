babc22324080c59be1e73dfde441518348abdb18
Tiago Loureiro <19423709+tiagoicp@users.noreply.github.com>
2024-03-19 11:16:27 +0000

    removed debug temp files

--- cmake-build-debug/lib/googletest-master/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/tiagoloureiro/dev/feup/DA2324_PRJ1_G11_3/lib/googletest-master
-# Build directory: /Users/tiagoloureiro/dev/feup/DA2324_PRJ1_G11_3/cmake-build-debug/lib/googletest-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- cmake-build-debug/lib/googletest-master/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/tiagoloureiro/dev/feup/DA2324_PRJ1_G11_3/lib/googletest-master/googlemock
-# Build directory: /Users/tiagoloureiro/dev/feup/DA2324_PRJ1_G11_3/cmake-build-debug/lib/googletest-master/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
